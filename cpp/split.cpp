7d4da62d9f2ad3789aa8b92d1746bac5c7be8906
David Egan <david@carawebs.com>
2018-03-09 20:49:41 +0000

    Tidy up.

--- cpp/split.cpp
+++ cpp/split.cpp
@@ -5,9 +5,6 @@
 
 int main(int argc, char const *argv[])
 {
-    // for (size_t i = 0; i < argc; i++) {
-    //     std::cout << "Argument " << i << " = " << argv[i] << std::endl;
-    // }
     std::string secret;
     int nShares;
     std::cout << "Please enter the secret that should be split:" << std::endl;
@@ -23,19 +20,6 @@ int main(int argc, char const *argv[])
         std::cerr << e.what() << std::endl;
         return 1;
     }
-
-    // getUserInput(secret, nShares);
-    // generateFragments(fragments, nShares, secret.length(), secret);
-
-    // std::cout << "Fragments" << std::endl;
-    // std::cout << std::string(80, '-') << std::endl;
-    // for(std::vector<int>& fragment : fragments) {
-    //     std::cout << toHexString(fragment) << std::endl;
-    // }
-    // std::cout << std::string(80, '-') << std::endl;
-    //
-    // // Overload toHexString()
-    // std::cout << "Secret, hex encoded plaintext: " << toHexString(secret) << std::endl;
 
     return 0;
 }
